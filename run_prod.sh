c7a1153beb3b4316d9b62a4b408e2ef8dc4b7f7c
Youngbok Yoon <master@bluehack.net>
2017-12-14 21:01:54 +0900

    Fixed container mismatched problem

--- run_prod.sh
+++ run_prod.sh
@@ -6,4 +6,4 @@ GRPC_PORT=30052
 sudo nvidia-docker run -dit --restart unless-stopped \
     -e RELEASE_MODE=$RELEASE_MODE \
     -e GRPC_PORT=$GRPC_PORT \
-    -p $GRPC_PORT:$GRPC_PORT bluelens/bl-search-vector:$RELEASE_MODE
+    -p $GRPC_PORT:$GRPC_PORT bluelens/bl-detect:$RELEASE_MODE
